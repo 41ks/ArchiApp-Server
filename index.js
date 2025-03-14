901ecfe5776b3f33d180cc0475213136421f33d9
Alex MELHEM <alex.melhem@student-cs.fr>
2025-03-14 14:25:36 +0100

    Test route

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express'); //import de la bibliothèque Express
 var app = express(); //instanciation d'une application Express
 
 // Pour s'assurer que l'on peut faire des appels AJAX au serveur
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -11,11 +11,13 @@ app.use(function(req, res, next) {
 // Ici faut faire faire quelque chose à notre app...
 // On va mettre les "routes"  == les requêtes HTTP acceptéés par notre application.
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.send("Hello")
 })
 
-
+app.get('/test/*', function (req, res) {
+  res.json({msg: req.url.split("/")[2]});
+})
 
 app.listen(8080); //commence à accepter les requêtes
 console.log("App listening on port 8080...");
